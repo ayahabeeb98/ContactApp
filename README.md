7d720d017f048b4a7f226563ed16ae49ad46cb70
Aya Habeeb <aya.r.h98@gmail.com>
2020-12-09 18:00:35 +0200

    add theme toggle feature and delete contact

--- README.md
+++ README.md
@@ -2,12 +2,20 @@
 ## Live Demo
 <a href="https://ayahabeeb98.github.io/ContactApp/"> click here </a>
 
-## Design preview
-![Design preview for the contact App](./contactPreview.png)
-
 ## Features:
 <ul>
     <li> Add a new contact </li>
     <li> Display contact details </li>
     <li> Search using email, phone, or name</li>
+    <li> Delete contact </li>
+    <li> Theme toggle </li>
 </ul>
+
+## Design preview
+**Light mode:**
+
+![Design preview for the contact App](./LightMode.png)
+
+**Dark mode:**
+
+![Design preview for the contact App](./DarkMode.png)
--- js/main.js
+++ js/main.js
@@ -3,16 +3,16 @@ let addBtn = document.querySelector('#addBtn');
 let addContactModal = document.querySelector('#addContact');
 let closeModalIcon = document.querySelector('.close');
 
-addBtn.addEventListener('click',()=>{
-    addContactModal.style.display="block"
+addBtn.addEventListener('click', () => {
+    addContactModal.style.display = "block"
 });
 
-closeModalIcon.addEventListener('click',()=>{
-    addContactModal.style.display="none";
+closeModalIcon.addEventListener('click', () => {
+    addContactModal.style.display = "none";
 
 });
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target === addContactModal) {
         addContactModal.style.display = "none";
     }
@@ -28,25 +28,26 @@ window.onclick = function(event) {
 //5- delete contact feature
 
 //upload and preview avatar
-let avatarFile = document.querySelector( '.form-file' );
+let avatarFile = document.querySelector('.form-file');
 const thump = document.querySelector('.avatar__thump');
 const noUserThump = './images/no-user.png';
 let newSrc = '' || noUserThump;
+
 function previewImage() {
     const thumpFile = avatarFile.files;
     if (thumpFile.length === 0) {
         thump.src = noUserThump
-    }else {
+    } else {
         newSrc = URL.createObjectURL(thumpFile[0]);
         thump.src = newSrc;
         thump.style.opacity = "1";
     }
 }
 
-avatarFile.addEventListener('change',previewImage);
+avatarFile.addEventListener('change', previewImage);
 
 //create a new contact
-function createNewContact(src,name,notes,key) {
+function createNewContact(src, name, notes, key) {
     const contactItem = document.createElement('DIV');
     contactItem.classList.add('contact__item');
     const avatarImage = document.createElement('IMG');
@@ -58,13 +59,18 @@ function createNewContact(src,name,notes,key) {
     contactName.textContent = name;
     const note = document.createElement('P');
     note.classList.add('contact__bio');
-    note.textContent = notes.slice(0,20);
+    note.textContent = notes.slice(0, 20);
+    const delUser = document.createElement('I');
+    delUser.className = 'fas fa-user-times';
+    delUser.setAttribute('data-id', key);
+
 
     contactInfo.appendChild(contactName);
     contactInfo.appendChild(note);
     contactItem.appendChild(avatarImage);
     contactItem.appendChild(contactInfo);
-    contactItem.setAttribute('data-key',key);
+    contactItem.appendChild(delUser);
+    contactItem.setAttribute('data-key', key);
     return contactItem;
 }
 
@@ -80,25 +86,28 @@ const contactBio = document.querySelector('#bio');
 
 //Store data in array of objects
 const contact = [
-    {name:"Aya Habeeb",
-    email:"aya.r.h98@gmail.com",
-    phone:"05900000000",
-    bio:"Lorem ipsum dolor sit ametconsectetur adipisicing elit. Magnam, sunt!",
-    key:0,
-    newSrc:noUserThump}
+    {
+        name: "Aya Habeeb",
+        email: "aya.r.h98@gmail.com",
+        phone: "05900000000",
+        bio: "Lorem ipsum dolor sit ametconsectetur adipisicing elit. Magnam, sunt!",
+        key: 0,
+        newSrc: noUserThump
+    }
 ];
 let k = 1;
-createContactBtn.addEventListener('click',(e)=>{
+
+createContactBtn.addEventListener('click', (e) => {
     e.preventDefault();
     const name = contactName.value;
     const email = contactEmail.value;
     const phone = contactPhone.value;
     const bio = contactBio.value;
     const key = k;
-    contact.push({name,email,phone,bio,key,newSrc});
-    wrapper.appendChild(createNewContact(newSrc,name,bio,key));
+    contact.push({name, email, phone, bio, key, newSrc});
+    wrapper.appendChild(createNewContact(newSrc, name, bio, key));
     addContactModal.style.display = "none";
-    k+=1;
+    k += 1;
     clearFields();
 });
 
@@ -112,16 +121,21 @@ function clearFields() {
 }
 
 
-
 //Display specific contact
-wrapper.addEventListener('click',e=>{
+wrapper.addEventListener('click', e => {
     let singleContact = e.target;
-    let contactKey = singleContact.dataset.key;
-    if (contactKey) {
-        let user = contact.filter(item => item.key === Number(contactKey));
-        showDetails(user[0])
-    }else {
-        console.log('something went wrong')
+    if (singleContact.dataset.id) {
+        let k = singleContact.dataset.id;
+        deleteUser(k)
+    } else {
+
+        let contactKey = singleContact.dataset.key;
+        if (contactKey) {
+            let user = contact.filter(item => item.key === Number(contactKey));
+            showDetails(user[0])
+        } else {
+            console.log('something went wrong')
+        }
     }
 });
 
@@ -145,7 +159,7 @@ function showDetails(user) {
 }
 
 
-closeContact.addEventListener('click',()=>{
+closeContact.addEventListener('click', () => {
     detailsWrapper.style.display = "none";
     no_user.style.display = "block";
     userImage.src = noUserThump;
@@ -155,45 +169,76 @@ closeContact.addEventListener('click',()=>{
 //Search Feature
 const searchKey = document.querySelector('#search');
 const searchBtn = document.querySelector('.search__btn');
-let filteredContact =[];
+let filteredContact = [];
 
 function searchContact(e) {
     e.preventDefault();
-    let cloneContact = contact.map(a => ({...a}));
-    let copyContact = cloneContact.map(item => {
-        delete item.newSrc;
-        delete item.key;
-        return item;
-    });
+    if (searchKey.value.trim().length > 0) {
 
-    for (let j=0;j < copyContact.length;j++){
-        let userInfo = Object.values(copyContact[j]);
-        for(let v=0; v <userInfo.length; v++){
-            if (userInfo[v].includes(searchKey.value.toLowerCase())){
-                console.log('find',userInfo[v]);
-                filteredContact.push(contact[j]);
-                break
+        let cloneContact = contact.map(a => ({...a}));
+        let copyContact = cloneContact.map(item => {
+            delete item.newSrc;
+            delete item.key;
+            return item;
+        });
+
+        for (let j = 0; j < copyContact.length; j++) {
+            let userInfo = Object.values(copyContact[j]);
+            for (let v = 0; v < userInfo.length; v++) {
+                if (userInfo[v].includes(searchKey.value.toLowerCase())) {
+                    filteredContact.push(contact[j]);
+                    break
+                }
             }
         }
+
+        wrapper.innerHTML = '';
+
+        filteredContact.forEach(item => {
+            wrapper.appendChild(createNewContact(item.newSrc, item.name, item.bio, item.key));
+        });
     }
-
-    wrapper.innerHTML = '';
-
-    filteredContact.forEach(item=>{
-        wrapper.appendChild(createNewContact(item.newSrc,item.name,item.bio,item.key));
-    });
 
 }
 
-searchBtn.addEventListener('click',searchContact);
+searchBtn.addEventListener('click', searchContact);
 
-searchKey.addEventListener('keyup',(e)=>{
-    if(e.target.value.trim() === ''){
+searchKey.addEventListener('keyup', (e) => {
+    if (e.target.value.trim() === '') {
         wrapper.innerHTML = '';
         filteredContact = [];
-        console.log(filteredContact);
-        contact.forEach(item=>{
-            wrapper.appendChild(createNewContact(item.newSrc,item.name,item.bio,item.key));
+        contact.forEach(item => {
+            wrapper.appendChild(createNewContact(item.newSrc, item.name, item.bio, item.key));
         });
     }
 });
+
+
+//Dark mode switcher
+
+const toggleMode = document.querySelector('#themeToggle');
+const modeIcon = document.querySelector('#modeIcon');
+
+let dark = false;
+toggleMode.addEventListener('click', () => {
+    modeIcon.className = dark ? "fas fa-moon" : "fas fa-sun";
+    document.querySelector('body').classList.toggle('dark--mode');
+    dark = !dark;
+});
+
+
+//Delete user
+
+function deleteUser(id) {
+    filteredContact = contact.filter(item => {
+        return String(item.key) !== id
+    });
+
+    wrapper.innerHTML = '';
+    filteredContact.forEach(item => {
+        wrapper.appendChild(createNewContact(item.newSrc, item.name, item.bio, item.key));
+    });
+
+    contact.splice(id,1);
+}
+
