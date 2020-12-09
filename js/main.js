0b20751519679086731c6d27d782d467a5dbaf89
Aya Habeeb <aya.r.h98@gmail.com>
2020-12-09 14:16:24 +0200

    add search feature

--- js/main.js
+++ js/main.js
@@ -21,8 +21,8 @@ window.onclick = function(event) {
 
 //TODO
 //Add contact script
-//1- Validate Form
-//2- create contact__item
+//1-
+//2- create co Validate Formntact__item
 //3- open contact__details
 //4- search contact feature
 //5- delete contact feature
@@ -58,7 +58,7 @@ function createNewContact(src,name,notes,key) {
     contactName.textContent = name;
     const note = document.createElement('P');
     note.classList.add('contact__bio');
-    note.textContent = notes;
+    note.textContent = notes.slice(0,20);
 
     contactInfo.appendChild(contactName);
     contactInfo.appendChild(note);
@@ -72,7 +72,6 @@ function createNewContact(src,name,notes,key) {
 //Add a new contact script
 
 const createContactBtn = document.querySelector('.btn-add');
-const contactList = document.querySelector('.contact__list');
 const wrapper = document.querySelector('.wrapper');
 const contactName = document.querySelector('#name');
 const contactEmail = document.querySelector('#email');
@@ -80,8 +79,15 @@ const contactPhone = document.querySelector('#phone');
 const contactBio = document.querySelector('#bio');
 
 //Store data in array of objects
-const contact = [];
-let k = 0;
+const contact = [
+    {name:"Aya Habeeb",
+    email:"aya.r.h98@gmail.com",
+    phone:"05900000000",
+    bio:"Lorem ipsum dolor sit ametconsectetur adipisicing elit. Magnam, sunt!",
+    key:0,
+    newSrc:noUserThump}
+];
+let k = 1;
 createContactBtn.addEventListener('click',(e)=>{
     e.preventDefault();
     const name = contactName.value;
@@ -142,4 +148,51 @@ closeContact.addEventListener('click',()=>{
     detailsWrapper.style.display = "none";
     no_user.style.display = "block";
     userImage.src = noUserThump;
+});
+
+
+//Search Feature
+const searchKey = document.querySelector('#search');
+const searchBtn = document.querySelector('.search__btn');
+let filteredContact =[];
+
+function searchContact(e) {
+    e.preventDefault();
+    let cloneContact = contact.map(a => ({...a}));
+    let copyContact = cloneContact.map(item => {
+        delete item.newSrc;
+        delete item.key;
+        return item;
+    });
+
+    for (let j=0;j < copyContact.length;j++){
+        let userInfo = Object.values(copyContact[j]);
+        for(let v=0; v <userInfo.length; v++){
+            if (userInfo[v].includes(searchKey.value.toLowerCase())){
+                console.log('find',userInfo[v]);
+                filteredContact.push(contact[j]);
+                break
+            }
+        }
+    }
+
+    wrapper.innerHTML = '';
+
+    filteredContact.forEach(item=>{
+        wrapper.appendChild(createNewContact(item.newSrc,item.name,item.bio,item.key));
+    });
+
+}
+
+searchBtn.addEventListener('click',searchContact);
+
+searchKey.addEventListener('keyup',(e)=>{
+    if(e.target.value.trim() === ''){
+        wrapper.innerHTML = '';
+        filteredContact = [];
+        console.log(filteredContact);
+        contact.forEach(item=>{
+            wrapper.appendChild(createNewContact(item.newSrc,item.name,item.bio,item.key));
+        });
+    }
 });
